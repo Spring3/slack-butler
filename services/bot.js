60532ccbf0e7be0c17e375b492f18316e6837beb
Daniyil Vasylenko <danielspr3@gmail.com>
2017-01-22 00:10:08 +0200

    GAdd scan command to scan the current channel for links

--- services/bot.js
+++ services/bot.js
@@ -1,7 +1,9 @@
-const { RtmClient } = require('@slack/client');
+const { RtmClient, WebClient } = require('@slack/client');
 const EVENTS = require('@slack/client').CLIENT_EVENTS;
+const request = require('request');
 
 const rtm = new RtmClient(process.env.SLACK_BOT_TOKEN);
+const web = new WebClient(process.env.SLACK_API_TOKEN);
 let writeChannel, botId;
 
 rtm.on(EVENTS.RTM.AUTHENTICATED, (data) => {
@@ -20,11 +22,10 @@ rtm.on(EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
   'Star, bot for hire', 
   'I usually do not work as a bot, but 20$ is 20$...', 
   'I believe I can touch the... Khm.. Star, at your service!', 
-  'Ouch, develo-bears!',
   'Hi everyone :blush:!',
   'Hey there!'];
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-  rtm.sendMessage(greeting, writeChannel);
+  // rtm.sendMessage(greeting, writeChannel);
 });
 
 rtm.on(EVENTS.RTM.WS_CLOSE, () => {
@@ -33,7 +34,7 @@ rtm.on(EVENTS.RTM.WS_CLOSE, () => {
 
 rtm.on(EVENTS.RTM.RAW_MESSAGE, (msg) => {
   msg = JSON.parse(msg);
-  if (msg.type === 'message') {
+  if (msg.type === 'message' && !msg.subtype) {
     const user = msg.user;
     const text = msg.text;
     const channel = msg.channel;
@@ -41,12 +42,55 @@ rtm.on(EVENTS.RTM.RAW_MESSAGE, (msg) => {
     if (text.indexOf('http://') >= 0 || text.indexOf('https://') >= 0) {
       rtm.sendMessage(`<@${user}>, I'll remember that.. :smiling_imp:`, channel);
     }
-    // mention greeting
+    // mention
     if (text.indexOf(`<@${botId}>`) >= 0) {
       const mentionedMessage = text.replace(`<@${botId}>`, '').toLowerCase().trim();
+      // greeting
       const greetings = ['hey', 'hi', 'hello', 'howdy', 'yo', 'greetings'];
       if (greetings.includes(mentionedMessage.replace(/[^\w\s]/gi, ''))) {
         rtm.sendMessage(`<@${user}>, hello, friend! :blush:`, channel);
+        return;
+      }
+
+      // api
+      // scan
+      switch (mentionedMessage) {
+        case 'scan': 
+          //rtm.sendMessage(`Scanning current channel for links and attachments...`, channel);
+          let len = 0;
+          const links;
+          const parse = (err, dataChunk) => {
+            const hasMore = dataChunk.has_more;
+            if (err) {
+              console.error(err);
+              throw err;
+            }
+            dataChunk = dataChunk.messages;
+            len += dataChunk.length;
+            if (hasMore) {
+              const lastTs = dataChunk[dataChunk.length - 1].ts;
+              request.get({
+                url: 'https://slack.com/api/channels.history',
+                qs: {
+                  token: process.env.SLACK_API_TOKEN,
+                  latest: lastTs,
+                  inclusive: 1,
+                  channel
+                },
+              }, function (err, response) {
+                  parse(err, JSON.parse(response.body));
+              });
+            }
+          }
+          web.channels.history(channel, parse);
+        break;
+        case 'help':
+          rtm.sendMessage(`Hi, <@${user}>! My name is Star - and my purpose is to save the links and attachments that people share in this channel.`, channel);
+          rtm.sendMessage('You may want to ask me for some of them. Simply ping me with one of the supported commands:' +
+          '```\n*list* - to display all saved items.\n*list n* - give a list of last n links saved.\n*scan* - to scan current channel for links and attachments.\n```', channel);
+          break;
+        default:
+
       }
     }
   }
