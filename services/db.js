84f0b95dd174aee359e0e28e809604d38e7cc417
Daniyil Vasylenko <danielspr3@gmail.com>
2017-01-22 19:34:15 +0200

    Enhanced list commands, saving links when in track mode

--- services/db.js
+++ services/db.js
@@ -6,23 +6,45 @@ class Database {
   static init() {
     return db.query('CREATE TABLE IF NOT EXISTS "links"(' +
       '"id" SERIAL PRIMARY KEY,' +
-      '"caption" varchar(1024) NOT NULL,' +
-      '"link" varchar(1024) NOT NULL UNIQUE);');
+      '"caption" varchar (256) NOT NULL,' +
+      '"link" TEXT NOT NULL UNIQUE);');
   }
 
   static insert(objArray) {
     const query = objArray.map((linkObj) => {
-      return `("${linkObj.caption}", "${linkObj.link}")`;
+      return `('${linkObj.caption}', '${linkObj.link}')`;
     }).reduce((sum, current) => {
       return sum.concat(`,${current}`);
     });
-    console.log(query);
-    let was, is;
-    return db.query('SELECT COUNT(*) FROM links;').then((count) => {
-      was = count;
+    let insertedAmount;
+    return db.query('SELECT COUNT(*) FROM links;').then((result) => {
+      insertedAmount = parseInt(result[0].count, 10);
       return db.query(`INSERT INTO links (caption, link) VALUES ${query} ON CONFLICT DO NOTHING;`);
     }).then((result) => {
-      console.log(result);
+      return db.query('SElECT COUNT(*) FROM links;');
+    }).then((result) => {
+      insertedAmount = parseInt(result[0].count, 10) - insertedAmount;
+      return insertedAmount;
+    });
+  }
+
+  static getTotalLinks() {
+    return db.query('SELECT COUNT(*) FROM links;').then((result) => {
+      return parseInt(result[0].count, 10);
+    });
+  }
+
+  static getLinks(limit, offset = 0) {
+    let query = 'SELECT caption, link FROM links';
+    if (limit) {
+      query += ` LIMIT ${limit}`;
+    }
+    console.log(offset);
+    query += ` OFFSET ${offset};`;
+    return db.many(query).then((result) => { 
+      return result.map((item) => {
+        return `[${item.caption}] - ${item.link}`;
+      }).join('\n');
     });
   }
 }
--- sql/links.sql
+++ sql/links.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS 'links'(
   'id' SERIAL PRIMARY KEY,
-  'caption' varchar(1024) NOT NULL,
-  'link' varchar(1024) NOT NULL UNIQUE
+  'caption' varchar(256) NOT NULL,
+  'link' TEXT NOT NULL UNIQUE
 );
