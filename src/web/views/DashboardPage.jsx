d1ede33c5f2e5eced53269b80458a0c2101f90a7
Daniyil Vasylenko <danielspr3@gmail.com>
2019-01-31 03:32:06 +0200

    Raw dashboard page contents

--- src/web/views/DashboardPage.jsx
+++ src/web/views/DashboardPage.jsx
@@ -3,17 +3,21 @@ import PropTypes from 'prop-types';
 import _ from 'lodash';
 import styled, { withTheme, css } from 'styled-components';
 import Navbar, { NavbarItem } from '../components/Navbar';
-
+import Section from '../components/Section';
+import MagnifyIcon from 'mdi-react/MagnifyIcon';
+import TuneIcon from 'mdi-react/TuneIcon';
+import FormatLineSpacingIcon from 'mdi-react/FormatLineSpacingIcon';
+import ViewGridIcon from 'mdi-react/ViewGridIcon';
 
 const ProfileTile = styled.div`
+  background-size: contain;
+  background-repeat: no-repeat;
+  padding-left: 30px;
   ${
     ({ img }) => css`
       background-image: url(${img});
     `
   }
-  background-size: contain;
-  background-repeat: no-repeat;
-  padding-left: 30px;
 `;
 
 class DashboardPage extends PureComponent {
@@ -29,7 +33,68 @@ class DashboardPage extends PureComponent {
           </NavbarItem>
           <NavbarItem>Log out</NavbarItem>
         </Navbar>
-
+        <Section>
+          <div>
+            <MagnifyIcon />
+            <div>
+              <span>
+                <TuneIcon />
+                Filters
+              </span>
+            </div>
+          </div>
+          <div>
+            <div>
+              <strong>Search Results (0)</strong>
+              <FormatLineSpacingIcon />
+              <ViewGridIcon />
+              <select>
+                <option>10 Per Pag</option>
+              </select>
+              <select>
+                <option>Date Down</option>
+              </select>
+            </div>
+            <div>
+              <a hre="#">
+                <img src="https://placeimg.com/250/150/tech" alt="preview" />
+                <p>Link name</p>
+                <div>
+                  <span>Channel: tech-resources</span>
+                  <span>Shared by: Daniel</span>
+                  <span>Added at: DEC 12 2018</span>
+                </div>
+              </a>
+              <a hre="#">
+                <img src="https://placeimg.com/250/150/tech" alt="preview" />
+                <p>Link name</p>
+                <div>
+                  <span>Channel: tech-resources</span>
+                  <span>Shared by: Daniel</span>
+                  <span>Added at: DEC 12 2018</span>
+                </div>
+              </a>
+              <a hre="#">
+                <img src="https://placeimg.com/250/150/tech" alt="preview" />
+                <p>Link name</p>
+                <div>
+                  <span>Channel: tech-resources</span>
+                  <span>Shared by: Daniel</span>
+                  <span>Added at: DEC 12 2018</span>
+                </div>
+              </a>
+              <a hre="#">
+                <img src="https://placeimg.com/250/150/tech" alt="preview" />
+                <p>Link name</p>
+                <div>
+                  <span>Channel: tech-resources</span>
+                  <span>Shared by: Daniel</span>
+                  <span>Added at: DEC 12 2018</span>
+                </div>
+              </a>
+            </div>
+          </div>
+        </Section>
       </Fragment>
     );
   }
